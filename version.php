d5f406ccd5c3ef99c7658055565beb1ab4868dd0
Yoshikazu Asada <yasada0819@users.noreply.github.com>
2026-02-21 13:43:35 +0900

    Update version.php
    increment version

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_dbviewrecorder';
-$plugin->version = 2026022102;
+$plugin->version = 2026022103;
 $plugin->requires = 2024042200; // Moodle 4.4 or later
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.3.2';
+$plugin->release = '0.3.3';
